1cd0812af66ed0467ec96b8a7f9410fc407baecd
Jonathan Rockway <jon@jrock.us>
2008-11-30 02:27:21 -0600

    initial import

---
+++ eproject.el
@@ -0,0 +1,144 @@
+;;; eproject
+
+(require 'iswitchb)
+(require 'cl)
+
+(defvar eproject-project-types nil
+  "An alist of PROJECT to (supertypes selector metadata-plist) pairs.")
+
+(defmacro define-project-type (type supertypes selector relevant-files)
+  "Define a new project type TYPE that inherits from SUPERTYPES.
+SELECTOR is a form that is given a filename FILE and returns the
+project root if it is of this type of project, or NIL otherwise.
+RELEVANT-FILES is a list of regular expressions that matches
+important files in this project type."
+  `(progn
+     (defvar ,(intern (format "%s-project-file-visit-hook" type)) nil
+       ,(format "Hooks that will be run when a file in a %s project is opened." type))
+       (setq eproject-project-types
+             (nconc (assq-delete-all ',type eproject-project-types)
+                    (list
+                     (list ',type ',supertypes
+                           (lambda (file) ,selector)
+                           (list :relevant-files ',relevant-files)))))))
+
+(defun eproject--scan-parents-for (start-at file)
+  "Look for a file named FILE in parent directories of START-AT"
+  (let ((try (expand-file-name (concat start-at "/" file))))
+    (cond ((file-exists-p try) start-at)
+          ((not (equal start-at "/"))
+           (eproject--scan-parents-for
+            (expand-file-name (concat start-at "/" "..")) file))
+          (t nil))))
+
+(define-project-type generic () nil ("^[^.]"))
+(define-project-type generic-git (generic) (look-for ".git") nil)
+
+(defun eproject--type-info (type)
+  (or
+   (assoc type eproject-project-types)
+   (error "No type %s" type)))
+
+(defun eproject--project-supertypes (type)
+  (nth 1 (eproject--type-info type)))
+
+(defun eproject--project-selector (type)
+  (nth 2 (eproject--type-info type)))
+
+(defun* eproject--run-project-selector (type &optional (file (buffer-file-name)))
+  (flet ((look-for (filename)
+                   (eproject--scan-parents-for file filename)))
+    (funcall (eproject--project-selector type) file)))
+
+
+(defun eproject--linearized-isa (type &optional include-self)
+  (delete-duplicates
+   (nconc
+    (if include-self (list type))
+    (eproject--project-supertypes type)
+    (loop for stype in (eproject--project-supertypes type)
+          nconc (eproject--linearized-isa stype)))))
+
+(defun eproject--all-types ()
+  '(generic-git generic))
+
+(defun eproject-get-project-metadatum (type key)
+  (loop for next-type in (eproject--linearized-isa type t)
+        do (let ((result (getf (nth 3 (eproject--type-info next-type)) key)))
+             (if result (return result)))))
+
+(defun eproject-add-project-metadatum (type key value)
+  (setf (getf (nth 3 (assoc type eproject-project-types)) key) value))
+
+(defvar eproject-root nil
+  "A buffer-local variable set to the root of its eproject project.  NIL if
+it isn't in an eproject.")
+
+(defvar eproject-type nil
+  "A buffer-local variable set to the type of this buffer's eproject project.  NIL
+if the buffer isn't in an eproject.")
+
+(make-variable-buffer-local 'eproject-root)
+(make-variable-buffer-local 'eproject-type)
+
+(define-minor-mode eproject-mode
+  "A minor mode for buffers that are a member of an eproject project."
+  nil " Project"
+  '(("" . eproject-ifind-file))
+  (when (null eproject-root)
+    (error "Please do not use this directly.  Call eproject-maybe-turn-on instead.")))
+
+(defun eproject-maybe-turn-on ()
+  "Turn on eproject for the current buffer, if it is in a project."
+  (interactive)
+  (loop for type in (eproject--all-types)
+        do (let ((root (eproject--run-project-selector type)))
+             (when root
+               (setq eproject-type type)
+               (setq eproject-root root)
+               (eproject-mode 1)
+               (return root)))))
+
+(defun eproject--search-directory-tree (directory file-regexp)
+  (let* ((content (directory-files (file-name-as-directory directory) t "^[^.]" t))
+         (files (loop for file in content
+                      when (and (not (file-directory-p file))
+                                (string-match file-regexp file))
+                      collect file))
+         (directories (loop for file in content
+                            when (file-directory-p file)
+                            collect file)))
+    (nconc files
+           (loop for dir in directories
+                 nconc (eproject--search-directory-tree dir file-regexp)))))
+
+(defun eproject--shorten-filename (filename)
+  (string-match (format "^%s/\\(.+\\)$" (regexp-quote eproject-root)) filename)
+  (cons (match-string 1 filename) filename))
+
+(defun eproject--icompleting-read (prompt choices)
+  "Use iswitch as a completing-read replacement to choose from
+choices.  PROMPT is a string to prompt with.  CHOICES is a list of
+strings to choose from."
+  (let ((iswitchb-make-buflist-hook
+         (lambda ()
+           (setq iswitchb-temp-buflist choices))))
+    (iswitchb-read-buffer prompt)))
+
+(defun eproject--icomplete-read-with-alist (prompt alist)
+  (let ((show (mapcar (lambda (x) (car x)) alist)))
+    (cdr (assoc (eproject--icompleting-read prompt show) alist))))
+
+(defun eproject-ifind-file ()
+  (interactive)
+  (let ((matcher (format "\\(?:%s\\)"
+                         (reduce (lambda (a b) (concat a "\\|" b))
+                                 (mapcar (lambda (f) (format "\\(?:%s\\)" f))
+                                         (eproject-get-project-metadatum
+                                          eproject-type :relevant-files))))))
+    (message matcher)
+    (eproject--icomplete-read-with-alist "Project file: "
+      (mapcar #'eproject--shorten-filename
+              (eproject--search-directory-tree eproject-root matcher)))))
+
+(add-hook 'find-file-hook #'eproject-maybe-turn-on)
