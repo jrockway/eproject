f41fc1309e3b9137e8faa15079dd6dc628a036e7
Antono Vasiljev <self@antono.info>
2012-05-25 17:32:20 +0300

    Improved ruby and rails projects
      - removed relevant files from rails project, only
        irrelevant should be ignored
      - add .ruby-version and .rvenv-version and *.gemspec as
        ruby project markers

--- lang/eproject-ruby.el
+++ lang/eproject-ruby.el
@@ -3,6 +3,10 @@
 (define-project-type ruby (generic)
   (or (look-for "Rakefile")
       (look-for "Gemfile")
-      (look-for ".rmvrc")))
+      (look-for "\.rmvrc")
+      (look-for "\.ruby-version")
+      (look-for "\.rbenv-version")
+      (look-for ".*\\.gemspec"))
+  :irrelevant-files (".*~"))
 
 (provide 'eproject-ruby)
